1d774ebf35cbdca805a97328fe38056e53754e94
Sarah Story <sarah.f.lavendol@vanderbilt.edu>
2015-08-11 14:58:43 -0500

    added rating system

--- javascripts/main.js
+++ javascripts/main.js
@@ -24,13 +24,22 @@ requirejs(["jquery", "bootstrap", "hbs", "firebase", "lodash", "add-movies"],
         console.log(movies);
         $('#movieList').html(movieTemplate(movies));
         $('div').filter('[seen="true"]').css('opacity', "0.4");
+        $('.rating').filter('[rating="2"]').addClass('two');
+        $('.rating').filter('[rating="4"]').addClass('four');
+        $('.rating').filter('[rating="6"]').addClass('six');
+        $('.rating').filter('[rating="8"]').addClass('eight');
+        $('.rating').filter('[rating="10"]').addClass('ten');
       });
     });
+
+
 
     $('#addMovie').click(function() {
       console.log('click');
       addMovies.addMovie();
     });
+
+    
 
     $( document ).on( "click", "#deleteButton", function() {
       var titleKey = $(this).parent().attr("key");
@@ -42,13 +51,18 @@ requirejs(["jquery", "bootstrap", "hbs", "firebase", "lodash", "add-movies"],
     $( document ).on( "click", "#okButton", function() {
       var watchedKey = $(this).parent().attr("key");
       var seenIt = new Firebase('https://movie-project.firebaseio.com/movies/' + watchedKey);
-      console.log('click');
       if ( $(this).parent().attr("seen") === "false" ) {
-
         seenIt.update({'seen-it': true});
       } else {
         seenIt.update({'seen-it': false});
-      };
+      }
+    });
+
+    $(document).on('click', '.rating span', function() {
+      var value = $(this).attr('value');
+      var starKey = $(this).parent().parent().attr('key');
+      var rating = new Firebase('https://movie-project.firebaseio.com/movies/' + starKey);
+      rating.update({'rating': value});
     });
 
   });
